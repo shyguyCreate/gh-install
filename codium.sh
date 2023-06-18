6e55c5b2834fadb8243c4f8c4ed9d7a3d5e46797
shyguyCreate <107062289+shyguyCreate@users.noreply.github.com>
2023-06-18 11:52:24 -0600

    Add: codium

---
+++ codium.sh
@@ -0,0 +1,93 @@
+#!/bin/sh
+
+#Add -f (force flag) to script
+forceFlag=false
+while getopts ":f" opt; do
+  case $opt in
+    f) forceFlag=true ;;
+    *) echo "Use -f to force installation" ;;
+  esac
+done
+
+#Reset getopts automatic variable
+OPTIND=1
+
+
+#Get latest tag_name
+if $forceFlag || [ ! -f /tmp/tag_name_codium ]
+then
+  curl -s https://api.github.com/repos/VSCodium/vscodium/releases/latest \
+  | grep tag_name \
+  | cut -d \" -f 4 \
+  | xargs > /tmp/tag_name_codium
+fi
+
+#Save tag_name to variable
+tag_name=$(cat /tmp/tag_name_codium)
+#Set the install directory with github tag added to its name
+installDir="/opt/codium $tag_name"
+
+
+#Get the current version of the program
+unset current_version; if [ -n "$(ls -d /opt/codium\ *)" ]
+then
+  current_version=$(basename /opt/codium\ * | awk '{print $2}')
+fi
+
+
+#Start installation if github version is not equal to installed version
+if $forceFlag || [ "$tag_name" != "$current_version" ]
+then
+  #Download binaries
+  curl -s https://api.github.com/repos/VSCodium/vscodium/releases/latest \
+  | grep "browser_download_url.*VSCodium-linux-x64-.*.tar.gz\"" \
+  | cut -d \" -f 4 \
+  | xargs curl -L -o /tmp/codium.tar.gz
+
+  #Remove contents if already installed
+  sudo rm -rf /opt/codium\ *
+
+  #Create folder for contents
+  sudo mkdir -p "$installDir"
+
+  #Expand tar file to folder
+  sudo tar zxf /tmp/codium.tar.gz -C "$installDir"
+
+  #Change execute permissions
+  sudo chmod +x "$installDir/bin/codium"
+
+  #Create symbolic link to bin folder
+  sudo ln -sf "$installDir/bin/codium" /usr/bin
+
+  #Add completions for bash
+  sudo mkdir -p /usr/share/bash-completion/completions
+  sudo cp "$installDir/resources/completions/bash/codium" /usr/share/bash-completion/completions
+
+  #Add completions for zsh
+  sudo mkdir -p /usr/share/zsh/site-functions
+  sudo cp "$installDir/resources/completions/zsh/_codium" /usr/share/zsh/site-functions
+else
+  echo "VSCodium is up to date."
+fi
+
+
+#Check if .desktop file exist
+if $forceFlag || [ ! -f /usr/share/applications/codium.desktop ]
+then
+  #Copy application image
+  sudo mkdir -p /usr/share/applications
+  sudo cp /opt/codium\ */resources/app/resources/linux/code.png /usr/share/pixmaps/codium.png
+
+  #Write application .desktop file
+  sudo mkdir -p /usr/share/pixmaps
+  echo \
+  '[Desktop Entry]
+  Name=VSCodium
+  Comment=Free/Libre Open Source Software Binaries of VS Code
+  GenericName=VSCodium
+  Exec=/usr/bin/codium
+  Icon=/usr/share/pixmaps/codium.png
+  Categories=Utility;TextEditor;Development;IDE
+  Type=Application' \
+  | sed 's/^[ \t]*//' - | sudo tee /usr/share/applications/codium.desktop > /dev/null
+fi
