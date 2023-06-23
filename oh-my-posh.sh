d2652502eea22251885e0472f6496d03d6b74f4c
shyguyCreate <107062289+shyguyCreate@users.noreply.github.com>
2023-06-23 06:15:09 -0600

    Add: oh-my-posh.sh

---
+++ oh-my-posh.sh
@@ -0,0 +1,87 @@
+#!/bin/sh
+
+program="Oh-My-Posh"
+
+#Add flags to script
+checkFlag=false
+forceFlag=false
+refreshFlag=false
+while getopts ":cfy" opt; do
+  case $opt in
+    c) checkFlag=true ;;
+    f) forceFlag=true ;;
+    y) refreshFlag=true ;;
+    *) echo "-c to check available updates"
+       echo "-f to force installation"
+       echo "-y to refresh github tag" ;;
+  esac
+done
+
+#Reset getopts automatic variable
+OPTIND=1
+
+
+#Get latest tag_name
+tag_tmp_file="/tmp/tag_name_oh-my-posh"
+if [ ! -f "$tag_tmp_file" ] || [ $refreshFlag = true ] || [ $forceFlag = true ]
+then
+  curl -s https://api.github.com/repos/JanDeDobbeleer/oh-my-posh/releases/latest \
+  | grep tag_name \
+  | cut -d \" -f 4 \
+  | xargs > "$tag_tmp_file"
+fi
+
+#Save tag_name to variable
+tag_name=$(cat "$tag_tmp_file")
+#Set the install directory with github tag added to its name
+installDir="/opt/oh-my-posh $tag_name"
+
+
+#Get the current version of the program
+current_version=$(find /opt -maxdepth 1 -type d -name "oh-my-posh *" -printf '%f' -quit | awk '{print $2}')
+
+
+#Start installation if github version is not equal to installed version
+if [ "$tag_name" != "$current_version" ] && [ $checkFlag = false ] || [ $forceFlag = true ]
+then
+  #Download binaries
+  curl -s https://api.github.com/repos/JanDeDobbeleer/oh-my-posh/releases/latest \
+  | grep "browser_download_url.*posh-linux-amd64\"" \
+  | cut -d \" -f 4 \
+  | xargs curl -L -o /tmp/oh-my-posh
+
+  #Remove contents if already installed
+  find /opt -maxdepth 1 -type d -name "oh-my-posh *" -exec sudo rm -rf '{}' \+
+
+  #Create folder for contents
+  sudo mkdir -p "$installDir"
+
+  #Copy binary file to folder
+  sudo cp /tmp/oh-my-posh "$installDir"
+
+  #Change execute permissions
+  sudo chmod +x "$installDir/oh-my-posh"
+
+  #Create symbolic link to bin folder
+  sudo mkdir -p /usr/local/bin
+  sudo ln -sf "$installDir/oh-my-posh" /usr/local/bin
+
+  #Add completions for bash
+  sudo mkdir -p /usr/local/share/bash-completion/completions
+  oh-my-posh completion -s bash | sudo tee /usr/local/share/bash-completion/completions/oh-my-posh > /dev/null
+
+  #Add completions for zsh
+  sudo mkdir -p /usr/local/share/zsh/site-functions
+  oh-my-posh completion -s zsh | sudo tee /usr/local/share/zsh/site-functions/_oh-my-posh > /dev/null
+
+elif [ $checkFlag = true ] && [ "$tag_name" = "$current_version" ]
+then
+  echo "Update not found for $program"
+
+elif [ $checkFlag = true ] && [ "$tag_name" != "$current_version" ]
+then
+  echo "Update found for $program"
+
+else
+  echo "$program is up to date"
+fi
